ceab8f99f3f0545ea5da53262aae0645b45e25cd
Dimitar Apostolovski <d.apostolovski@gmail.com>
2018-10-05 23:19:17 +0200

    basics of UI - HTML, (messy) CSS - added

--- code.js
+++ code.js
@@ -14,4 +14,20 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, operandA, operandB) {
+  switch (operator) {
+    case '+':
+      return add(operandA, operandB);
+    case '-':
+      return subtract(operandA, operandB);
+    case '*':
+      return multiply(operandA, operandB);
+    case '/':
+      return divide(operandA, operandB);
+    default:
+      throw new Error("Unknown operation: " + operator);
+  }
+}
+
 [add, subtract, multiply, divide].forEach(f => console.log(f(3, 5)));
+['+', '-', '*', '/'].forEach(o => console.log(operate(o, 6, 4)));
