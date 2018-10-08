ff9a40dbaeb19e4dc7cee8aafb575eae1f7e1bcd
Dimitar Apostolovski <d.apostolovski@gmail.com>
2018-10-08 19:23:20 +0200

    'lcd' display completed

--- code.js
+++ code.js
@@ -56,7 +56,22 @@ document.querySelectorAll('.digit, .dot').forEach(div => div.addEventListener('c
 let display_element = document.querySelector('#display');
 
 function updateDisplay(value) {
-  display_element.textContent = value;
+  value = '' + value;
+  let dot_position = value.indexOf('.');
+  if (dot_position != -1) value = value.slice(0, dot_position) + value.slice(dot_position + 1);
+  value = value.slice(0,11);
+  for (let i=0; i<digit_displays.length; i++){
+    if (i >= value.length) {
+      //digit_displays[i].textContent = '';
+      digit_displays[i].classList.remove('active');
+    } else {
+      digit_displays[i].textContent = value.charAt(value.length - 1 - i);
+      digit_displays[i].classList.add('active');
+    }
+    //
+    dot_displays[i].classList.remove('active');
+  }
+  if (dot_position != -1) dot_displays[value.length - dot_position].classList.add('active');
 }
 
 function operatorListener(event) {
@@ -92,3 +107,31 @@ function controlListener(event) {
 }
 
 document.querySelectorAll('#clear, #delete').forEach(div => div.addEventListener('click', controlListener));
+
+let digit_displays = [];
+let dot_displays = [];
+for (let i = 10; i >= 0; i--){
+  // digits
+  let div = document.createElement('div');
+  let static_span = document.createElement('span');
+  static_span.textContent = 8;
+  div.appendChild(static_span);
+  let display_span = document.createElement('span');
+  display_span.classList.add('output');
+  digit_displays[i] = display_span;
+  div.appendChild(display_span);
+  display_element.appendChild(div);
+  // dots
+   div = document.createElement('div');
+   static_span = document.createElement('span');
+  static_span.textContent = '.';
+  div.appendChild(static_span);
+   display_span = document.createElement('span');
+   display_span.textContent = '.';
+  display_span.classList.add('output');
+  dot_displays[i] = display_span;
+  div.appendChild(display_span);
+  display_element.appendChild(div);
+}
+
+updateDisplay(0);
